cc82b07ebfa0bde36012a862763ed3ff17132f6f
Christian Dupuis <cd@atomist.com>
2018-07-16 08:37:53 +0200

    Add test sdm configuration

---
+++ test/atomist.config.ts
@@ -0,0 +1,29 @@
+import { Configuration } from "@atomist/automation-client";
+import { SoftwareDeliveryMachine } from "@atomist/sdm";
+import {
+    configureSdm,
+    createSoftwareDeliveryMachine,
+} from "@atomist/sdm-core";
+import { SoftwareDeliveryMachineConfiguration } from "@atomist/sdm/api/machine/SoftwareDeliveryMachineOptions";
+import { FingerprintSupport } from "../src";
+
+export function machineMaker(configuration: SoftwareDeliveryMachineConfiguration): SoftwareDeliveryMachine {
+
+    const sdm = createSoftwareDeliveryMachine({
+        name: `${configuration.name}-test`,
+        configuration,
+    });
+
+    // put in other things you need for your test
+
+    sdm.addExtensionPacks(FingerprintSupport);
+
+    return sdm;
+
+}
+
+export const configuration: Configuration = {
+    postProcessors: [
+        configureSdm(machineMaker),
+    ]
+}
