35f62f69a7e5bb30f2705e3297bb479f9a0deb29
slimslenderslacks <slimslenderslacks@gmail.com>
2018-07-18 23:39:37 -0700

    Add the skeletal set of generic command handlers needed for the library goal lifecycle.

---
+++ src/handlers/commands/pushImpactCommandHandlers.ts
@@ -0,0 +1,121 @@
+import { Parameters, Parameter, MappedParameter, Value, MappedParameters, Secret } from "@atomist/automation-client";
+import { CommandHandlerRegistration, CommandListenerInvocation } from "../../../node_modules/@atomist/sdm";
+
+@Parameters()
+export class IgnoreVersionParameters {
+
+    @Parameter({ required: false, displayable: false })
+    public msgId?: string;
+
+    @MappedParameter(MappedParameters.GitHubOwner)
+    public owner: string;
+
+    @MappedParameter(MappedParameters.GitHubRepository)
+    public repo: string;
+
+    @MappedParameter(MappedParameters.GitHubRepositoryProvider)
+    public providerId: string;
+
+    @Value("name")
+    public name: string;
+
+    @Value("version")
+    public version: string;
+}
+
+@Parameters()
+export class SetTeamLibraryGoalParameters {
+
+    @Parameter({ required: false, displayable: false })
+    public msgId?: string;
+
+    @Value("name")
+    public name: string;
+
+    @Value("version")
+    public version: string;
+}
+
+@Parameters()
+export class ShowGoalsParameters {
+
+    @MappedParameter(MappedParameters.GitHubOwner)
+    public owner: string;
+
+    @MappedParameter(MappedParameters.GitHubRepository)
+    public repo: string;
+
+    @MappedParameter(MappedParameters.GitHubRepositoryProvider)
+    public providerId: string;
+
+    @Secret("github://user_token?scopes=repo")
+    public userToken: string;
+}
+
+@Parameters()
+export class ConfirmUpdateParameters {
+
+    @Parameter({ required: false, displayable: false })
+    public msgId?: string;
+
+    @MappedParameter(MappedParameters.GitHubOwner)
+    public owner: string;
+
+    @MappedParameter(MappedParameters.GitHubRepository)
+    public repo: string;
+
+    @MappedParameter(MappedParameters.GitHubRepositoryProvider)
+    public providerId: string;
+
+    @Value("name")
+    public name: string;
+
+    @Value("version")
+    public version: string;
+}
+
+function ignoreVersion(cli: CommandListenerInvocation<IgnoreVersionParameters>) {
+    return;
+}
+
+function setTeamLibraryGoal(cli: CommandListenerInvocation<SetTeamLibraryGoalParameters>) {
+    return;
+}
+
+function confirmUpdate(cli: CommandListenerInvocation<ConfirmUpdateParameters>) {
+    return;
+}
+
+function showGoals(cli: CommandListenerInvocation<ShowGoalsParameters>) {
+    cli.context.messageClient.respond("okay, I'll do it");
+    return;
+}
+
+export const IgnoreVersion: CommandHandlerRegistration<IgnoreVersionParameters> = {
+    name: "LibraryImpactIgnoreVersion",
+    description: "Allow a Project to skip one version of library goal",
+    paramsMaker: IgnoreVersionParameters,
+    listener: async cli => ignoreVersion(cli),
+};
+
+export const SetTeamLibrary: CommandHandlerRegistration<SetTeamLibraryGoalParameters> = {
+    name: "LibraryImpactSetTeamLibrary",
+    description: "set a new target for a team to consume a particular version",
+    paramsMaker: SetTeamLibraryGoalParameters,
+    listener: async cli => setTeamLibraryGoal(cli),
+};
+
+export const ConfirmUpdate: CommandHandlerRegistration<ConfirmUpdateParameters> = {
+    name: "LibraryImpactConfirmUpdate",
+    description: "choose to raise a PR on the current project for a library version update",
+    paramsMaker: ConfirmUpdateParameters,
+    listener: async cli => confirmUpdate(cli),
+};
+
+export const ShowGoals: CommandHandlerRegistration<ShowGoalsParameters> = {
+    name: "LibraryImpactShowGoals",
+    description: "show the current goals for this team",
+    intent: "get library targets",
+    paramsMaker: ShowGoalsParameters,
+    listener: async cli => showGoals(cli),
+};
--- src/machine/FingerprintSupport.ts
+++ src/machine/FingerprintSupport.ts
@@ -9,20 +9,23 @@ import {
 import { metadata } from "@atomist/sdm/api-helper/misc/extensionPack";
 import { PushImpactHandler } from "../handlers/events/pushImpactHandler";
 import { allDeps } from "../npm/deps";
-import { File } from "../../node_modules/@atomist/automation-client/project/File";
-import { Fingerprint } from "../../node_modules/@atomist/automation-client/project/fingerprint/Fingerprint";
-
-const fp: Fingerprint = {name: "npm-project-deps", 
-                         version: "0.0.1",
-                         sha: "",
-                         abbreviation: "",
-                         data: ""};
+import { File } from "@atomist/automation-client/project/File";
+import { Fingerprint } from "@atomist/automation-client/project/fingerprint/Fingerprint";
+import {MavenFingerprinter} from "@atomist/sdm-pack-spring";
+import { IgnoreVersion, SetTeamLibrary, ShowGoals } from "../handlers/commands/pushImpactCommandHandlers";
+import { ConfirmUpdate } from "../handlers/commands/pushImpactCommandHandlers";
 
 const npmProjectDeps: PushImpactListener<FingerprinterResult> = 
     async (i: PushImpactListenerInvocation) => {
         try {
-            let f: File = await i.project.findFile("package.json");
-            return [fp];
+            const f: File = await i.project.findFile("package.json");
+            const data: string = clj.fingerprintPackageJson(f);
+            return [{"name": "npm-project-deps",
+                     "version": "0.0.1",
+                     "abbreviation": "npm-deps",
+                     "sha": clj.sha256(data),
+                     "data": data,
+                     "value": data}];
         } catch (err){
             return [];
         }
@@ -33,6 +36,11 @@ export const FingerprintSupport: ExtensionPack = {
     configure: sdm => {
 
         sdm.addEvent(PushImpactHandler);
+
+        sdm.addCommand(IgnoreVersion);
+        sdm.addCommand(ConfirmUpdate);
+        sdm.addCommand(SetTeamLibrary);
+        sdm.addCommand(ShowGoals);
 
         sdm.addFingerprinterRegistration({
                 name: "clj-fingerprinter",
@@ -46,6 +54,7 @@ export const FingerprintSupport: ExtensionPack = {
             .addFingerprinterRegistration(
                 {name: "npm-fingerprinter",
                  action: npmProjectDeps})
+            .addFingerprinterRegistration(new MavenFingerprinter())
             .addFingerprinterRegistration({
                 name: "js-fingerprinter",
                 action: async (i: PushImpactListenerInvocation) => {
