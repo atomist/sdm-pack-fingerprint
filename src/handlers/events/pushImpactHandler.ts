c474afdc9c8b0bfba4e157549b0178eb893ad788
Jim Clark <slimslenderslacks@gmail.com>
2018-07-20 19:58:40 -0700

    Impact diff rendering is working

--- src/handlers/events/pushImpactHandler.ts
+++ src/handlers/events/pushImpactHandler.ts
@@ -6,15 +6,16 @@ import {
 } from "@atomist/automation-client";
 import { subscription } from "@atomist/automation-client/graph/graphQL";
 import { OnEvent } from "@atomist/automation-client/onEvent";
-import { buttonForCommand } from "@atomist/automation-client/spi/message/MessageClient";
+import { buttonForCommand, SlackFileMessage } from "@atomist/automation-client/spi/message/MessageClient";
 import * as impact from "@atomist/clj-editors";
 import { SlackMessage } from "@atomist/slack-messages";
 import { PushImpactEvent, GetFingerprintData } from "../../typings/types";
 import { EventHandlerRegistration } from "@atomist/sdm";
 import { NoParameters } from "@atomist/automation-client/SmartParameters";
 import { QueryOptions } from "@atomist/automation-client/internal/graph/graphQL";
-import { QueryNoCacheOptions } from "../../../node_modules/@atomist/automation-client/spi/graph/GraphClient";
-import _ = require("../../../node_modules/@types/lodash");
+import { QueryNoCacheOptions } from "@atomist/automation-client/spi/graph/GraphClient";
+import _ = require("lodash");
+import * as clj from "@atomist/clj-editors";
 
 function qcon(ctx:HandlerContext, diff: impact.Diff): void {
     const message: SlackMessage = {
@@ -44,50 +45,56 @@ function forFingerprint(s:string): (fp: impact.FP) => boolean {
    }
 }
 
+function getFingerprintDataCallback(ctx: HandlerContext): (sha:string, name:string) => Promise<string> {
+    return (sha:string, name:string):Promise<string> => {
+        return ctx.graphClient.query<GetFingerprintData.Query, GetFingerprintData.Variables>({
+            name: "get-fingerprint",
+            variables: {
+                sha: sha,
+                name: name
+            },
+            options: QueryNoCacheOptions,
+        })
+        .then(result => {
+            logger.info(`getFingerprintData:  got successful result ${result}`);
+            const fingerprints =
+                _.get(result, "Commit[0].fingerprints") as GetFingerprintData.Fingerprints[];
+            if (fingerprints) {
+                return fingerprints[0].data as string; 
+            }
+            return "{}";
+        })
+        .catch( (reason) => {
+            logger.info(`error getting fingerprint data ${reason}`);
+            return "{}"
+        });
+    }
+}
+
+function renderDiffSnippet(ctx: HandlerContext, diff: impact.Diff): void {
+    // TODO SlackFileMessages seem to not be sendable currently
+    ctx.messageClient.addressChannels( clj.renderDiff(diff), diff.channel);
+}
+
+function checkLibraryGoals(ctx: HandlerContext, diff: impact.Diff): void {
+    
+}
+
 const PushImpactHandle: OnEvent<PushImpactEvent.Subscription> =
     (event: EventFired<PushImpactEvent.Subscription>, ctx: HandlerContext) => {
-
         logger.info("handler PushImpactEvent subscription");
-        const fingerprintData = (sha:string, name:string) => {
-            return ctx.graphClient.query<GetFingerprintData.Query, GetFingerprintData.Variables>({
-                name: "getFingerprintData",
-                variables: {
-                    sha: sha,
-                    name: name
-                },
-                options: QueryNoCacheOptions,
-            })
-            .then(result => {
-                const fingerprints =
-                    _.get(result, "Commit[0].fingerprints") as GetFingerprintData.Fingerprints[];
-                if (fingerprints) {
-                    return fingerprints[0].data as string; 
-                }
-                return "{}";
-            })
-            .catch( (reason) => {
-                logger.info(`error getting fingerprint data ${reason}`);
-                return "{}"
-            });
-        }
         impact.processPushImpact(
             event,
-            fingerprintData,
+            getFingerprintDataCallback(ctx),
             [ 
-                {
-                    selector: forFingerprint("fingerprint1"),
-                    diffAction: (diff: impact.Diff) => {
-                        logger.info(`check diff from ${diff.from.sha} to ${diff.to.sha}`);
-                        qcon(ctx, diff);
-                    },
-                },
                 {
                     selector: forFingerprint("npm-project-deps"),
                     action: (diff: impact.Diff) => {
                         logger.info(`check for goal diffs here`);
+                        checkLibraryGoals(ctx,diff);
                     },
                     diffAction: (diff: impact.Diff) => {
-                        logger.info(`check npm-project-deps from ${diff.from.sha} to ${diff.to.sha}`);
+                        renderDiffSnippet(ctx,diff);
                     },
                 }
             ]
