146e8040c2c89ae948edaaa519fb304317747b30
slimslenderslacks <slimslenderslacks@gmail.com>
2019-05-22 23:59:15 -0700

    turn back on the metadata filter

--- src/atomist/public_defns.cljs
+++ src/atomist/public_defns.cljs
@@ -119,7 +119,7 @@
        (take-while (complement m/end?))
        (filter (is-sym 'defn))
        (filter start-of-list?)
-       #_(filter fingerprint-metadata?)
+       (filter fingerprint-metadata?)
        (map (partial public-sig clj-path))
        (filter identity)))
 
