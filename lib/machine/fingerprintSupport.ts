42447d50af4bfa9f05362ea3d6fb838827ddae15
James Carnegie <james@atomist.com>
2019-08-19 16:06:24 +0100

    Revert "Resolve unbounded/side-effecting Promise[] serially"
    This reverts commit 1d6e2d292bae490f22325c4bbe1fea7617bfca84.

--- lib/machine/fingerprintSupport.ts
+++ lib/machine/fingerprintSupport.ts
@@ -63,7 +63,6 @@ import {
     setTargetFingerprintFromLatestMaster,
     updateTargetFingerprint,
 } from "../handlers/commands/updateTarget";
-import { promiseAllSeq } from "../support/util";
 import {
     Aspect,
     FingerprintDiffHandler,
@@ -104,14 +103,16 @@ export const DefaultTargetDiffHandler: FingerprintDiffHandler =
     async (ctx, diffs, aspect) => {
         const abs = _.filter(diffs, diff => !diff.from);
         return [
-            ... await promiseAllSeq(
+            ... await Promise.all(
                     _.map (
                         _.difference(diffs, abs), diff => checkFingerprintTarget(
                             ctx.context,
                             diff,
                             aspect,
                             async () => diff.targets ))),
-            ... _.map(abs, () => ({abstain: true})),
+            ... _.map(abs, () => {
+                return {abstain: true};
+             }),
         ];
     };
 
@@ -125,7 +126,9 @@ export function diffOnlyHandler(handler: FingerprintDiffHandler): FingerprintDif
         const toDiff = _.filter(diffs, diff => diff.from && diff.to.sha !== diff.from.sha);
         return [
             ... await handler(context, toDiff, aspect),
-            ... _.map(_.difference(diffs, toDiff), () => ({abstain: true})),
+            ... _.map(_.difference(diffs, toDiff), () => {
+                return {abstain: true};
+             }),
         ];
     };
 }
